e28b651d0d791f5335d9b67506a0ab316cc7f0f3
Saymon Porras Briones <118936420+CtpN3m01@users.noreply.github.com>
2024-08-08 17:56:19 -0600

    arreglado

--- Prototipo1/src/main/java/main/Prototipo1.java
+++ Prototipo1/src/main/java/main/Prototipo1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package main.prototipo_1;
+package main;
 
 /**
  *
  * @author saimo
  */
-public class Prototipo_1 {
+public class Prototipo1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
