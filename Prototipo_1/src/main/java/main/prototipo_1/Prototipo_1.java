fcccca43ab6cb93c327d2772a51b057c151bae0b
Saymon Porras Briones <sporras@estudiantec.cr>
2024-08-08 17:24:18 -0600

    update

--- Prototipo_1/src/main/java/main/prototipo_1/Prototipo_1.java
+++ Prototipo_1/src/main/java/main/prototipo_1/Prototipo_1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.prototipo1;
+package main.prototipo_1;
 
 /**
  *
  * @author saimo
  */
-public class Prototipo1 {
+public class Prototipo_1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
