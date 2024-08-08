bed21670ab9252453adf342d9cf92c992dd580d8
Saymon Porras Briones <sporras@estudiantec.cr>
2024-08-08 16:24:49 -0600

    Archivos Java Prototipo 1

---
+++ Prototipo1/src/main/java/com/mycompany/prototipo1/Prototipo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prototipo1;
+
+/**
+ *
+ * @author saimo
+ */
+public class Prototipo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
